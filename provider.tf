aa954112fdfa66e56f7afedebd3e4450fbc44a1f
Yogesh <yogesh0387@gmail.com>
2026-06-24 12:45:48 +0200

    added tf backened s3

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-yy-13"
+    key            = "global/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
